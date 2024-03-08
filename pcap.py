05619c8b6c3782c8bd6bacbc52ed548c8ffeedd2
Rukongai <joseph@josephrankin.com>
2024-03-08 16:36:15 -0700

    cleanup

--- pcap.py
+++ pcap.py
@@ -1,11 +1,11 @@
 import pyshark
 
-cap = pyshark.FileCapture('pcap\pcap.pcapng')
+cap = pyshark.FileCapture('./lepcap.pcapng')
 
 with open('output.txt', 'a') as output_file:
     for pkt in cap:
         try:
-            if pkt.length == "1448" and pkt.ip.dst == 'YOUR LOCAL MACHINE IP' and pkt.transport_layer == "UDP":
+            if pkt.length == "1448" and pkt.ip.dst == 'YOUR MACHINE IP HERE' and pkt.transport_layer == "UDP":
                 nextpk = (int(pkt.number))
                 listingpt1 = (pkt.udp.payload).replace(':', '')
                 listingpt2 = (cap[nextpk].udp.payload).replace(':', '')
